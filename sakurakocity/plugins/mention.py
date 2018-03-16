4faa91aef95c251414a1cd3fa1f98a47da54c5ce
yu-nadawaki <yu-nadawaki@lacras.co.jp>
2018-03-16 10:29:15 +0900

    initialize.

--- sakurakocity/plugins/mention.py
+++ sakurakocity/plugins/mention.py
@@ -7,6 +7,6 @@ import random
 def listen(message):
     message.reply('くれ')
 
-@respond_to('お手')
+@respond_to('お手|おて｜おかわり|たて|立て')
 def respond(message):
-    message.reply(random.choice(['ん？', 'ぽむむ〜？']))
+    message.reply(random.choice(['ん？', 'ぽむむ〜？', 'お手だぞ', 'おかわりだぞ', '立てだぞ', 'もっと立てだぞ']))
